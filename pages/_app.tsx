125e90eb16f26426e71031d33bbad6ee46d645b0
Javier Álvarez Medina <xavijam@gmail.com>
2021-10-14 14:55:37 +0200

    Using finally Tinybird provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,42 @@
 import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
-import TinybirdProvider from 'next-tinybird'
 
 import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
+import TinybirdProvider, { useTinybird } from 'next-tinybird'
+import { useRouter } from 'next/router'
 
 const Noop: FC = ({ children }) => <>{children}</>
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
+  const tinybird = useTinybird()
+
+  const router = useRouter()
+  
+  useEffect(() => {
+    tinybird('pageload', { url: router.pathname })
+
+    const handleRouteChange = (url: string) => {
+      tinybird('pageload', { url })
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [])
 
   useEffect(() => {
     document.body.classList?.remove('loading')
   }, [])
 
   return (
-    <TinybirdProvider domain={'https://api.tinybird.co'} dataSource={'events'} token={'hello'}>
+    <TinybirdProvider api="https://api.tinybird.co" trackerURL={'https://storage.googleapis.com/tinybird-demo/tinybird-tracker.js'} dataSource={'events'} token={'token'}>
       <Head />
       <ManagedUIContext>
         <Layout pageProps={pageProps}>
