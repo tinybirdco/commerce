d35e3b714e03f79bc9117aaa01a0870f857d4e72
Javier Álvarez Medina <xavijam@gmail.com>
2021-10-15 15:31:55 +0200

    Added admin link

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -32,6 +32,9 @@ const Navbar: FC<NavbarProps> = ({ links }) => (
                 <a className={s.link}>{l.label}</a>
               </Link>
             ))}
+            <Link href="/admin">
+              <a className={s.link}>Admin</a>
+            </Link>
           </nav>
         </div>
         {process.env.COMMERCE_SEARCH_ENABLED && (
