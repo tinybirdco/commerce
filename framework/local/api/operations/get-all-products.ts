69775256408f58cd2422b9b8f214832ef3374c7a
Raquel Yuste <raquel@tinybird.co>
2022-01-26 10:10:25 +0100

    get related products from local

--- framework/local/api/operations/get-all-products.ts
+++ framework/local/api/operations/get-all-products.ts
@@ -2,9 +2,7 @@ import { Product } from '@commerce/types/product'
 import { GetAllProductsOperation } from '@commerce/types/product'
 import type { OperationContext } from '@commerce/api/operations'
 import type { LocalConfig, Provider } from '../index'
-
-const API_URL = process.env.NEXT_PUBLIC_TINYBIRD_API
-const API_TOKEN = process.env.NEXT_PUBLIC_TINYBIRD_TOKEN
+import data from '../../data.json'
 
 export default function getAllProductsOperation({
   commerce,
@@ -19,28 +17,10 @@ export default function getAllProductsOperation({
     config?: Partial<LocalConfig>
     preview?: boolean
   } = {}): Promise<{ products: Product[] | any[] }> {
-    const url = `${API_URL}/v0/pipes/basic_data.json?token=${API_TOKEN}`
-
-    const { res, error } = await fetch(url)
-      .then((res) => res.json())
-      .then((res) => ({ res }))
-      .catch((error) => ({ error: error.toString() }))
-
-    if (res?.data) {
-      const { data } = res
-      const products = data.map((product) => ({
-        ...product,
-        path: `/${product.path}`,
-      }))
-
-      return {
-        products: variables?.first
-          ? products.slice(0, variables?.first)
-          : products,
-      }
-    } else {
-      console.log(error)
-      return Promise.reject(error)
+    return {
+      products: variables?.first
+        ? data.products.slice(0, variables?.first)
+        : data.products,
     }
   }
   return getAllProducts
