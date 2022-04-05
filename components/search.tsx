5a10691c877bf15cc5036a67611b5f96e7ac6995
gnzjgo <gonzalojgo@gmail.com>
2022-04-05 16:37:01 +0200

    cities to categories just in case

--- components/search.tsx
+++ components/search.tsx
@@ -88,7 +88,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
               <span className="rounded-md shadow-sm">
                 <button
                   type="button"
-                  onClick={(e) => handleClick(e, 'cities')}
+                  onClick={(e) => handleClick(e, 'categories')}
                   className="flex justify-between w-full rounded-sm border border-accent-3 px-4 py-3 bg-accent-0 text-sm leading-5 font-medium text-accent-4 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8 transition ease-in-out duration-150"
                   id="options-menu"
                   aria-haspopup="true"
@@ -164,7 +164,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                           }}
                         >
                           <a
-                            onClick={(e) => handleClick(e, 'cities')}
+                            onClick={(e) => handleClick(e, 'categories')}
                             className={
                               'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
                             }
